22b96ee8d83c0b1e2673c395aa708c6abcf2bf69
tunsay <tuna1997@hotmail.fr>
2022-08-02 20:29:02 +0200

    feat: Implement close modal
    closes #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,19 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelectorAll(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalClose.forEach((btn) => btn.addEventListener("click", close));
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+//close modal event while press x
+function close() {
+  modalbg.style.display = "none";
+}
 
