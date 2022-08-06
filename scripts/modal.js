970db0ddbc3b450b1449db0c39ffd1963b6ab884
tunsay <tuna1997@hotmail.fr>
2022-08-06 20:53:44 +0200

    Correction of layout error message

--- scripts/modal.js
+++ scripts/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelectorAll(".close");
+const form = document.querySelector('form');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -30,79 +31,134 @@ function close() {
   modalbg.style.display = "none";
 }
 
-//Verification form
-function checkEmail(email) {
-  var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  return re.test(email);
+// form data elements
+var firstName = document.forms["reserve"]["first"];
+var lastName = document.forms["reserve"]["last"];
+var email = document.forms["reserve"]["email"];
+var birthdate = document.forms["reserve"]["birthdate"];
+var quantity = document.forms["reserve"]["quantity"];
+var conditions = document.forms["reserve"]["checkbox1"];
+
+//error messages
+const errorMessages = {
+  firstNameError: "Vous devez entrer 2 caractères ou plus.",
+  lastNameError: "Vous devez entrer 2 caractères ou plus.",
+  emailError: "L'adresse email est invalide.",
+  birthdateError: "La date de naissance est invalide.",
+  quantityError: "Veuillez entrer un nombre valide entre 0 et 99.",
+  locationError: "Vous devez sélectionner une ville.",
+  conditionsError: "Vous devez accepter les conditions d'utilisations."
+};
+
+
+// checks first name & returns true if not empty and has 2 or more characters
+function firstNameValid() {
+  let nameInput = firstName.value;
+  return nameInput !== null && nameInput.length >= 2;
 }
 
+// checks last name & returns true if not empty and has 2 or more characters
+function lastNameValid() {
+  let lastNameInput = lastName.value;
+  return lastNameInput !== null && lastNameInput.length >= 2;
+}
+
+//checks for correct email format
+function emailValid() {
+  let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let emailInput = email.value;
+  if (emailInput.match(regexEmail)) return true;
+}
+
+// checks that only numbers between 0 and 99 are entered
+function quantityValid() {
+  if (quantity.value !== '' && quantity.value >= 0 && quantity.value <= 99)
+    return true;
+}
+
+//checks for correct birthdate
+function birthdateValid() {
+  let regexBirthdate = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
+  return regexBirthdate.test(birthdate.value);
+}
+
+// checks that a location has been selected
+function locationValid() {
+  // gets all radio inputs
+  let radioButtons = document.forms["reserve"]["location"];
+  // loops through all radio inputs, returns true if one is checked
+  for (let radio of radioButtons) {
+    if (radio.checked === true) return true;
+  }
+}
+
+// checks that the required box is checked
+function conditionsValid() {
+  return conditions.checked;
+}
+
+
+
+// displays error message when field is invalid
+function isInvalid(i, message) {
+  formData[i].setAttribute("data-error-visible", "true");
+  formData[i].setAttribute("data-error", message);
+}
+
+// removes error message on valid fields
+function isValid() {
+  let invalidInput = document.querySelectorAll('.formData[data-error-visible="true"]');
+  for (let input of invalidInput) {
+    input.setAttribute("data-error-visible", "false");
+    input.setAttribute("data-error", "");
+  }
+}
+
+
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  // validate();
+});
+
+// checks every field, displays error message when invalid
 function validate() {
-  var firstName = document.forms["reserve"]["first"];
-  var lastName = document.forms["reserve"]["last"];
-  var email = document.forms["reserve"]["email"];
-  //vérification de la date
-  var date_regex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-  //Invers format yyyy-mm-dd to dd-mm-yyyy
-  var birthdate = document.forms["reserve"]["birthdate"].value.split("-").reverse().join("-");
-  var quantity = document.forms["reserve"]["quantity"];
-  var location = document.forms["reserve"]["location"];
-
-  console.log("Le prénom:")
-  console.log(firstName.value);
-  console.log("Le nom:")
-  console.log(lastName.value);
-  console.log("L'email':")
-  console.log(email.value);
-  console.log("La date d'anniversaire:")
-  console.log(birthdate);
-  console.log("Le nombre de concours participé:")
-  console.log(quantity.value);
-  console.log("Le lieu:")
-  console.log(location.value);
-
-  if (firstName.value == "" && firstName.value.length < 3) {
-    alert("Le champ Prénom a un minimum de 2 caractères / n'est pas vide.");
-    firstName.focus();
-    return false;
+  let formValid = true;
+  // clear the ancient message error
+  isValid();
+  if (!firstNameValid()) {
+    isInvalid(0, errorMessages.firstNameError);
+    formValid = false;
   }
-
-  if (lastName.value == "" && lastName.value.length < 3) {
-    alert("Le champ Nom a un minimum de 2 caractères / n'est pas vide.");
-    lastName.focus();
-    return false;
+  if (!lastNameValid()) {
+    formValid = false;
+    isInvalid(1, errorMessages.lastNameError);
   }
-
-  if (checkEmail(email.value)) {
-  } else {
-    alert('Adresse e-mail non valide');
-    email.focus();
-    return false;
+  if (!emailValid()) {
+    formValid = false;
+    isInvalid(2, errorMessages.emailError);
   }
-
-  if (date_regex.test(birthdate)) {
-  } else {
-    alert("Date non valide");
-    return false;
+  if (!birthdateValid()) {
+    formValid = false;
+    isInvalid(3, errorMessages.birthdateError);
   }
-
-  if (quantity.value == "" || isNaN(quantity.value) || quantity.value <= 0 || quantity.value > 100) {
-    alert("Veuillez rentrer un nombre valide et de préférence entre 0 et 99");
-    quantity.focus();
-    return false;
+  if (!quantityValid()) {
+    formValid = false;
+    isInvalid(4, errorMessages.quantityError);
   }
-
-  if (location.value == ""){
-    alert("Veuillez sélectionner unn lieu !");
-    return false;
+  if (!locationValid()) {
+    formValid = false;
+    isInvalid(5, errorMessages.locationError);
   }
-  
-  if (document.getElementById("checkbox1").checked == false)
-	{
-		alert('Veuillez coché la case svp !');
-    return false;
-	}
-
-  alert("Tout est ok !");
-
-  return true;
+  if (!conditionsValid()) {
+    formValid = false;
+    isInvalid(6, errorMessages.conditionsError);
+  }
+  if (formValid) {
+    console.log(firstName.value);
+    alert("Tout est ok !");
+    close();
+  }
 }
+
+
